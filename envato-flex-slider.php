2fa67124ed7110eae3312426b82f92b7cec2ea08
John Fred Fadrigalan <johnfred.fadrigalan@hotmail.com>
2024-09-15 17:40:28 +0800

    Add changes from gwt-wordpress

--- envato-flex-slider.php
+++ envato-flex-slider.php
@@ -1,100 +1,85 @@
 <?php
-
 /*
 Plugin Name: Envato FlexSlider
-Plugin URI: 
+Plugin URI:
 Description: A simple plugin that integrates FlexSlider (http://flex.madebymufffin.com/) with WordPress using custom post types!
 Author: Joe Casabona
-Version: 0.8
+Version: 0.5
 Author URI: http://www.casabona.org
 */
 
 /*Some Set-up*/
-define('EFS_PATH', WP_PLUGIN_URL . '/' . plugin_basename( dirname(__FILE__) ) . '/' );
+define('EFS_PATH', get_template_directory_uri() . '/inc/vendors/' . basename( dirname(__FILE__) ) . '/' );
 define('EFS_NAME', "Envato FlexSlider");
-define ("EFS_VERSION", "0.8");
+define ("EFS_VERSION", "0.5");
 
 /*Files to Include*/
 require_once('slider-img-type.php');
 
-
-/*Add the Javascript/CSS Files!*/
-wp_enqueue_script('flexslider', EFS_PATH.'jquery.flexslider-min.js', array('jquery'));
-wp_enqueue_style('flexslider_css', EFS_PATH.'flexslider.css');
-
-
-/*Add the Hooks to place the javascript in the header*/
-
-function efs_script(){
-
-print '<script type="text/javascript" charset="utf-8">
-  jQuery(window).load(function() {
-    jQuery(\'.flexslider\').flexslider();
-  });
-</script>';
-
-}
-
-add_action('wp_head', 'efs_script');
-
 function efs_get_slider(){
-
-/**Options Array...to be added on admin later...*/
-$showTitle= true;
-$showText= true;
-$beforeTitle= "<strong>";
-$afterTitle= "</strong>"; 
-	
-	$slider= '<div class="flexslider contain">
-	  <ul class="slides">';
-
-	$efs_query= "post_type=slider-image";
+	$efs_query = "post_type=slider-image";
 	query_posts($efs_query);
 	
+	global $post_id;
 	
-	if (have_posts()) : while (have_posts()) : the_post(); 
-		$img= get_the_post_thumbnail( $post->ID, 'large' );
+	if (have_posts()) : 	
 		
-		$slider.='<li>'.$img;
-		if($showTitle || $showText){
-			$slider.= '<p class="flex-caption">';
-			$slider.= ($showTitle) ? $beforeTitle. get_the_title() .$afterTitle : '';
-			$slider.= ($showText) ? '<br/>'.get_the_content() : ''; 
-			$slider.='</p>';
-		}
-		$slider.= '</li>';
+		$slider = '<div class="orbit" role="region" aria-label="Banner Slider" data-orbit data-options="animInFromLeft:fade-in; animInFromRight:fade-in; animOutToLeft:fade-out; animOutToRight:fade-out;">
+					<ul class="orbit-container">';		
+		
+		$count = 0;
+		$x = 1;
+		while (have_posts()) : the_post();
+			$count++;
+		endwhile;
+
+		while (have_posts()) : the_post();
+			$img = get_the_post_thumbnail($post_id, 'full', array( 'class' => 'orbit-image' ));
 			
-	endwhile; endif; wp_reset_query();
+  		$slide_link = slider_link_get_meta_box_data(get_the_ID());
+			$caption = get_the_title();
 
+			if ($x > $count) {
+				$x = 1;
+			}
+			$slider.= $post_id.'<li class="orbit-slide is-active"><div class="orbit-slide-number"><span>'.$x.'</span> of <span>'.$count.'</span></div><a href="'.$slide_link.'">'.$img.'</a><figcaption class="orbit-caption">'.$caption.'</figcaption></li>';
+			$x++;
+		endwhile;
 
-	$slider.= '</ul>
-	</div>';
-	
+		if($count > 1) {
+			$slider .= '<button class="orbit-previous"><span class="show-for-sr">Previous Slide</span>&#9664;&#xFE0E;</button>
+						<button class="orbit-next"><span class="show-for-sr">Next Slide</span>&#9654;&#xFE0E;</button>';
+		}
+	endif;
+	wp_reset_query();
+
+	if($count > 1) {
+		$slider .= '</ul>
+		<nav class="orbit-bullets">';
+
+		for ($x=0; $x < $count ; $x++) { 
+			$class = ($x == 0) ? 'is-active' : '' ;
+			$slider .= '<button class="'.$class.'" data-slide="'.$x.'"><span class="show-for-sr">Current Slide</span></button>';
+		}
+
+		$slider .= '</div></nav>';
+	} else {
+		$slider .= '</ul></div>';
+	}
+
 	return $slider;
 }
 
-
 /**add the shortcode for the slider- for use in editor**/
-
-function efs_insert_slider($atts, $content=null){
-
-$slider= efs_get_slider();
-
-return $slider;
-
+function efs_insert_slider($atts, $content=null) {
+	$slider= efs_get_slider();
+	return $slider;
 }
-
-
 add_shortcode('ef_slider', 'efs_insert_slider');
 
-
-
 /**add template tag- for use in themes**/
-
-function efs_slider(){
-
+function efs_slider() {
 	print efs_get_slider();
 }
-
 
 ?>
